372241c463fdf3ce047ad85cc814ef70065d7cb4
szc107544 <18646202718@163.com>
2016-12-06 10:04:40 +0800

    创建新的视图控制器NewViewController

---
+++ iOSszc_git_Demo/iOSszc_git_Demo/NewCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  NewCollectionViewCell.h
+//  iOSszc_git_Demo
+//
+//  Created by SZC on 2016/12/6.
+//  Copyright © 2016年 孙志超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ iOSszc_git_Demo/iOSszc_git_Demo/NewCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  NewCollectionViewCell.m
+//  iOSszc_git_Demo
+//
+//  Created by SZC on 2016/12/6.
+//  Copyright © 2016年 孙志超. All rights reserved.
+//
+
+#import "NewCollectionViewCell.h"
+
+@implementation NewCollectionViewCell
+
+@end
